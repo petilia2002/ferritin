af67ea0c44eb9b44f0897b37594482e658f45094
Ilya Penrenckov <petilia2002@yandex.ru>
2024-12-21 14:45:43 +0300

    small refactor..

--- read_json.py
+++ read_json.py
@@ -5,17 +5,14 @@ with open("./output/labs.json", "r", encoding="utf-8") as file:
     statistic = json.load(file)
 
 rows = []
-for measure, labs in statistic.items():
-    for lab_id, stats in labs.items():
-        row = {"measure": measure, "lab_id": lab_id}
-        row.update(stats)
+for measure, stats in statistic.items():
+    for lab, estimations in stats.items():
+        row = {"measure": measure, "lab_id": lab}
+        row.update(estimations)
         rows.append(row)
 
 df = pd.DataFrame(rows)
-df = df[["measure", "lab_id", "min", "max", "mean", "std"]]
-
-table = df.to_string(index=False)
-print(table)
+txt_string = df.to_string(index=True)
 
 with open("./output/labs.txt", "w", encoding="utf-8") as file:
-    file.write(table)
+    file.write(txt_string)
---
+++ test2.py
@@ -0,0 +1,4 @@
+dict_c = {"a": 1, "b": 2}
+updates = [("b", 3), ("c", 4)]  # список кортежей
+dict_c.update(updates)
+print(dict_c)
