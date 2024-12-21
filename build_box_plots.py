c99cb1270fb556baae4c12440d50a040cec30444
Ilya Penrenckov <petilia2002@yandex.ru>
2024-12-21 23:32:51 +0300

    small refactor..

--- build_box_plots.py
+++ build_box_plots.py
@@ -26,7 +26,6 @@ labs_id = np.unique(df["lab_id"].values.ravel())
 print(labs_id)
 print(labs_id.size)
 
-z_score = 1.96
 for g in range(len(labels) // 4):
     rows, cols = 2, 2
     fig, axes = plt.subplots(rows, cols, figsize=(12, 6))
@@ -36,40 +35,37 @@ for g in range(len(labels) // 4):
             axes[i, j].set_title(f"Box-plot для каждой лаборатории", pad=10)
             axes[i, j].set_xlabel(f"Значение {parameter}", labelpad=1)
 
+            data_to_plot = []
+            positions = []
+            labs = []
             for k in range(labs_id.size):
-                lab = f"lab_id{labs_id[k]}"
-                data = df[df["lab_id"] == labs_id[k]].filter(items=[parameter])
+                lab_id = labs_id[k]
+                lab = f"lab_id{lab_id}"
+                data = df[df["lab_id"] == lab_id].filter(items=[parameter])
                 data.dropna(inplace=True, ignore_index=True)
 
-                min_value = float((data[parameter].min()))
-                max_value = float((data[parameter].max()))
-                mean_value = float((data[parameter].mean()))
                 std_value = float((data[parameter].std()))
-
-                low, high = (
-                    mean_value - z_score * std_value,
-                    mean_value + z_score * std_value,
-                )
-                # data = data[(data[parameter] >= low) & (data[parameter] <= high)]
 
                 if math.isnan(std_value):
                     print(
-                        f"Дисперсия NaN у показателя {parameter} в лаборатории {labs_id[k]}"
+                        f"Дисперсия NaN у показателя {parameter} в лаборатории {lab_id}"
                     )
                     continue
+                data_to_plot.append(data[parameter])
+                positions.append(k + 1)
+                labs.append(f"Лаб.{lab_id}")
 
-                axes[i, j].boxplot(
-                    data[parameter],
-                    vert=False,
-                    patch_artist=True,
-                    boxprops=dict(facecolor="lightblue"),
-                    tick_labels=[f"Лаб.{labs_id[k]}"],
-                )
-            # axes[i, j].legend(loc="upper right", fontsize="small", framealpha=0.5)
+            axes[i, j].boxplot(
+                data_to_plot,
+                positions=positions,
+                vert=False,
+                patch_artist=True,
+                boxprops=dict(facecolor="lightblue"),
+                showfliers=False,
+            )
+            axes[i, j].set_yticks(positions)
+            axes[i, j].set_yticklabels(labs)
+            axes[i, j].grid(visible=True, linestyle="--", alpha=0.7)
     plt.subplots_adjust(hspace=0.5)
-    """
-    plt.savefig(
-        f"./browse_data_images/lab_dist_page_{g + 1}.png", dpi=300, bbox_inches="tight"
-    )
-    """
+    plt.savefig(f"./browse_data_images/box_plots_page_{g + 1}.png", dpi=300)
     plt.show()
